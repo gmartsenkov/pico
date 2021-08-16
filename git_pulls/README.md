6b899cff2cd7c9edfb9010b6aa5250ab8bf2958c
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-08-16 01:53:51 +0100

    add init git_pulls project

---
+++ git_pulls/README.md
@@ -0,0 +1,3 @@
+## Features
+[ ] - Fetch github pull requests information
+[ ] - Display the list of pull requests on a characted LCD display
---
+++ git_pulls/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <pico/stdlib.h>
+
+int main() 
+{
+    stdio_init_all();
+    const uint led_pin = 25;
+
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+
+    while (true) 
+    {
+        printf("Blink!\r\n");
+        gpio_put(led_pin, true);
+        sleep_ms(1000);
+        gpio_put(led_pin, false);
+        sleep_ms(1000);
+    }
+}
